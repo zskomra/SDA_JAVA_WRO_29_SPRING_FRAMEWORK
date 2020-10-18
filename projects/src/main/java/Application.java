796e36d36d4b1175c37340622e0e7a0f81ac80aa
z.skomra <z.skomraa@gmail.com>
2020-10-18 18:19:45 +0200

    Task A.2.1 Bean configuration with xml

--- projects/src/main/java/Application.java
+++ projects/src/main/java/Application.java
@@ -1,9 +1,15 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 import warmup.HelloWorld;
 
 public class Application {
     public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        HelloWorld bean = context.getBean(HelloWorld.class);
+        bean.sayHello();
 
+    }
+    private static void genericWarmUp() {
         GenericApplicationContext context = new GenericApplicationContext();
         context.registerBean("helloWorld", HelloWorld.class);
         context.refresh();
@@ -17,6 +23,5 @@ public class Application {
         bean = context.getBean(HelloWorld.class);
         bean.sayHello();
     }
-
 
 }
